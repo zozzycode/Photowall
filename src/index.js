c37901cd0cc3a989294a1f297d61c0a01c275e3e
zozzycode <zozzycode@gmail.com>
2018-04-29 14:51:09 +0300

    Comments Added
    Comments Added

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/stylesheet.css';
 import { BrowserRouter } from 'react-router-dom'; // builds URL
-import { createStore, applyMiddleware } from 'redux'; // Creating Store and middleware
-import rootReducer from './redux/reducer'; // Reducer
-import { Provider } from 'react-redux'; //  link state with an App
-import App from './Components/App'; // Inject data to the Main component
-import thunk from 'redux-thunk';
-import { database } from './database/config';
+import { createStore, applyMiddleware } from 'redux'; // Creating Store, async requests to DB
+import rootReducer from './redux/reducer'; // State changer
+import { Provider } from 'react-redux'; //  passes state to the App component
+import App from './Components/App'; // main wrapper. injects data to the Main
+import thunk from 'redux-thunk'; // async actions to DB
+import { database } from './database/config'; // DB
 
-const store = createStore(rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(thunk)
+// create store
+const store = createStore(rootReducer, // first arg - reducer. Always one
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // chrome redux webtool setup string
+  applyMiddleware(thunk) // async actions to DB
 );
 
-ReactDOM.render(
-  <Provider store={store}>
-    <BrowserRouter>
+ReactDOM.render( // ReactDOM.render(what, where)
+  <Provider store={store}> // passes state to the App component
+    <BrowserRouter> // builds URL
       <App/>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root'));
+  document.getElementById('root')); // where
